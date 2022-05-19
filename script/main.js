3d1711bb76a2756de005607010654d7dae49aac4
Skaletti <gordey.korolev.2016@mail.ru>
2022-05-19 19:18:25 +0700

    FF-3: main-hero added

---
+++ script/main.js
@@ -0,0 +1,14 @@
+new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+});
