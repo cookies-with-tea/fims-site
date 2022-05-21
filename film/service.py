d6f0afe81bdf8a2eb77336045475f260873b0c29
bookslomot <alexgol2101@gmail.com>
2022-05-22 00:51:53 +0400

    FS-4 fix film.service

--- film/service.py
+++ film/service.py
@@ -2,7 +2,7 @@ from userlibrary.models import UserFilm
 
 
 def average_rating(is_film):
-    all_rating, count = 0, 0
+    all_rating = 0
     user_f = UserFilm.objects.filter(film=is_film).values('rating')
     count = len(user_f)
     for i in user_f:
