30e9fa185ba8d485b890ee29f777ff1519573279
Skaletti <gordey.korolev.2016@mail.ru>
2022-12-16 11:46:04 +0700

    Authoriztion/registration

---
+++ src/components/HeroSlider.vue
@@ -0,0 +1,38 @@
+<template>
+  <swiper
+    :cssMode="true"
+    :navigation="true"
+    :pagination="true"
+    :mousewheel="true"
+    :keyboard="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+  </swiper>
+</template>
+
+  <script lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+
+
+
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, Mousewheel, Keyboard],
+    };
+  },
+};
+</script>
