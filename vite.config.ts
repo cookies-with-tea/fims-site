579ffb6fde8c464761d09d402c319762001d6a00
Nermitor <dmatyukhin2006@mail.ru>
2023-10-18 01:10:39 +0700

    Создал Базу

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+// import EslintPlugin from 'vite-plugin-eslint';
+// import StyleLintPlugin from 'vite-plugin-stylelint';
+// import AutoImport from 'unplugin-auto-import/vite';
+
+import { defineConfig, loadEnv } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+
+// const styleLintConfig = StyleLintPlugin({
+//   files: ['src/**/*.{vue,scss}'],
+//   fix: true,
+// });
+
+// const eslintConfig = EslintPlugin({
+//   fix: true,
+//   cache: false,
+// });
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') as ImportMetaEnv;
+
+  return {
+    plugins: [vue(),
+      // styleLintConfig, eslintConfig, autoImportConfig, componentsConfig, svgIconsConfig, fontsConfig
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/resources" as *; @use "@/styles/vendor" as *;',
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
+});
