002ae4d45d771030077d9debe31db4842e7aaad0
Nermitor <dmatyukhin2006@mail.ru>
2023-11-05 13:38:06 +0700

    fix: lf

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
-import { svgIconsPlugin, styleLintPlugin, eslintPlugin } from './plugins';
+import { eslintPlugin, styleLintPlugin, svgIconsPlugin } from './plugins';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
