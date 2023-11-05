e53b9f3574970449bbb0d8f824f509473bcc988f
Nermitor <dmatyukhin2006@mail.ru>
2023-11-06 00:49:14 +0700

    update: Чекбоксу добавлена надпись

--- src/components/ui/BaseCheckbox.vue
+++ src/components/ui/BaseCheckbox.vue
@@ -1,39 +1,48 @@
 <template>
-  <input
-    class="base-checkbox"
-    type="checkbox"
-    :value="modelValue"
-    @change="$emit('update:modelValue', $event.target?.checked)"
-  />
+  <div class="base-checkbox">
+    <input
+      v-bind="$attrs"
+      class="checkbox base-checkbox__box"
+      type="checkbox"
+      :value="modelValue"
+      v-on="$attrs"
+      @change="$emit('update:modelValue', $event.target?.checked)"
+    />
+    <div v-if="props.label" class="base-checkbox__label">
+      {{ props.label }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 type Props = {
   modelValue: boolean;
+  label?: string;
 };
 
 type Emits = {
   (e: 'update:modelValue', value: boolean): void;
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
 
 defineEmits<Emits>();
 </script>
 
 <style scoped lang="scss">
 @supports (-webkit-appearance: none) or (-moz-appearance: none) or (appearance: none) {
-  input.base-checkbox[type='checkbox'] {
+  input.checkbox[type='checkbox'] {
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
   }
 }
 
-.base-checkbox {
+.checkbox {
   width: 31px;
   height: 31px;
   position: relative;
+  display: block;
   border-radius: 8px;
   background-color: $base-color--whit;
 
@@ -48,6 +57,20 @@ defineEmits<Emits>();
 
   &:focus {
     box-shadow: 0 0 0 2px rgb(39 94 254 / 0.25);
+  }
+}
+
+.base-checkbox {
+  display: inline-flex;
+  align-items: center;
+  justify-content: left;
+
+  &__box {
+    margin-right: 12px;
+  }
+
+  &__label {
+    color: $color--checkbox;
   }
 }
 </style>
