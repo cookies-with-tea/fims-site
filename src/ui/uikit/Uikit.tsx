0938be19188e269863f04c1690e6f6a08f479942
Andrey <andre.kozloff2004@gmail.com>
2023-12-17 15:44:22 +0300

    ASF-2: сделал uikit страницу

--- src/ui/uikit/Uikit.tsx
+++ src/ui/uikit/Uikit.tsx
@@ -1,14 +1,105 @@
+import { useState } from "react"
 import { Button } from "../Button/Button"
-// import { Icon } from "../icon/Icon"
-// import { Input } from "../input/input"
+import { Icon } from "../icon/Icon"
+import { Input } from "../input/input"
+import stl from "./uikit.module.scss"
 
 
 export const Uikit = () => {
+    const [value, setValue] = useState("")
     return (
-        <div className="uikit">
+        <div className={stl.uikit}>
+            <div className={stl.df}>
+                <Button>
+                    Вход
+                </Button>
+
+                <Button 
+                    variant="secondary" 
+                    size="sm">
+                    Вход
+                </Button>
+
+                <Button size="xs" 
+                        radius="max">
+                    Вход
+                </Button>
+
+                <Button postfixIcon={<Icon name="filter"/>}>
+                    Вход
+                </Button>
+
+                <Button prefixIcon={<Icon name="filter"/>}>
+                    Вход
+                </Button>
+
+                <Button disabled={true}>
+                    Вход
+                </Button>
+            </div>
+
+            <div style={{margin:"20px 0"}}>
+                <Button 
+                    prefixIcon={<Icon name="filter"/>} 
+                    href="/">
+                    Вход
+                </Button>
+            </div>
+
+            <div style={{fontSize:"30px"}}>
+                <Icon name="filter"/>
+
+                <Icon name="clear"/>
+
+                <Icon name="eye-off"/>
+
+                <Icon name="eye-on"/>
+            </div>
+
             <div>
-                <Button>Вход</Button>
-                <Button variant="secondary">Вход</Button>
+                <Input 
+                    value={value} 
+                    placeholder="вход"
+                    onChange={setValue}
+                />
+                <Input 
+                    value={value} 
+                    placeholder="вход"
+                    onChange={setValue}
+                    clearable={true}
+                    size="sm"
+                />
+                <Input 
+                    value={value} 
+                    placeholder="вход"
+                    onChange={setValue}
+                    size="xs"
+                    type="password"
+                />
+
+                <Input 
+                    value={value} 
+                    placeholder="вход"
+                    onChange={setValue}
+                    postfixIcon={<Icon name="filter"/>}
+                    prefixIcon={<Icon name="filter"/>}
+                />
+
+                <Input 
+                    value={value} 
+                    placeholder="вход"
+                    onChange={setValue}
+                    append={"Open"}
+                    prepend={"Open"}
+                />
+            </div>
+            <div>
+                <Input 
+                    type="textarea"
+                    placeholder="вход"
+                    value={value}
+                    onChange={setValue}
+                    />
             </div>
         </div>
     )
