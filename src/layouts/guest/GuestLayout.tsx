3740a053ffab6a22f38d5103ff4a3f127dc7db2e
Andrey <andre.kozloff2004@gmail.com>
2023-12-27 19:05:20 +0300

    ASF-3: убрал лишние стили для лейлаута

--- src/layouts/guest/GuestLayout.tsx
+++ src/layouts/guest/GuestLayout.tsx
@@ -4,9 +4,7 @@ import style from "./GuestLayou.module.scss"
 export const GuestLayout = () => {
     return (
         <div className={style.auth}>
-            <div className={style.auth__container}>
-                <Outlet/>
-            </div>
+            <Outlet/>
         </div>
     )
 }
