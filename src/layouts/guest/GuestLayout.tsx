96ca4e9f12df32c9608b6756d0e4b1ab644504b6
Andrey <andre.kozloff2004@gmail.com>
2023-12-29 19:20:44 +0300

    ASF-3: добавил дополнительную обертку для лейаута

--- src/layouts/guest/GuestLayout.tsx
+++ src/layouts/guest/GuestLayout.tsx
@@ -4,7 +4,9 @@ import style from "./GuestLayou.module.scss"
 export const GuestLayout = () => {
     return (
         <div className={style.auth}>
-            <Outlet/>
+            <div className={style.auth__body}>
+                <Outlet/>
+            </div>
         </div>
     )
 }
--- src/pages/login/PageLogin.tsx
+++ src/pages/login/PageLogin.tsx
@@ -23,68 +23,66 @@ export const PageLogin = () => {
     const onValueChange: InputChangeEventHandler = (event) => {
         setFormData({ ...formData, [event.target.name]: event.target.value})
     }
-
     return (
-        <form className={cx("form")}>
-            <div className={cx("form__title")}>
-                Вход
-            </div>
+        <>
+            <h3 className={cx("auth__title")}>Вход</h3>
+            <form className={cx("form")}>
+                <div className={cx("form__inner")}>
+                    <div className={cx("form__item")}>
+                        <label className={cx("form__label")}>Почта или имя</label>
+                        <Input 
+                            value={formData.email}
+                            placeholder='Email or username'
+                            onChange={onValueChange}
+                            onClearValue={onClearValue}
+                            // DEBT: в дальнешем пересмотреть надобность функции onClearValue
+                            clearable={true}
+                            name='email'
+                            />
+                    </div>
 
-            <div className={cx("form__inner")}>
-                <div className={cx("form__item")}>
-                    <label className={cx("form__label")}>Почта или имя</label>
-                    <Input 
-                        value={formData.email}
-                        placeholder='Email or username'
-                        onChange={onValueChange}
-                        onClearValue={onClearValue}
-                        // DEBT: в дальнешем пересмотреть надобность функции onClearValue
-                        clearable={true}
-                        name='email'
+                    <div className={cx("form__item")}>
+                        <label className={cx("form__label")}>Пароль</label>
+                        <Input 
+                            value={formData.password}
+                            placeholder='Password'
+                            onChange={onValueChange}
+                            type='password'
+                            name='password'
                         />
+                    </div>
                 </div>
-
-                <div className={cx("form__item")}>
-                    <label className={cx("form__label")}>Пароль</label>
-                    <Input 
-                        value={formData.password}
-                        placeholder='Password'
-                        onChange={onValueChange}
-                        type='password'
-                        name='password'
-                    />
+                
+                <div className={cx("form__btn")}>
+                    <Button
+                        type='submit' 
+                        radius='max'
+                        >
+                        Создать аккаунт
+                    </Button>
                 </div>
-            </div>
-            
-            <div className={cx("form__btn")}>
-                <Button
-                    type='submit' 
-                    radius='max'
-                    >
-                    Создать аккаунт
-                </Button>
-            </div>
-            
-            <div className={cx("form__entrance")}>
-                <div className={cx("form__subtitle")}>
-                    Или войти через
+                
+                <div className={cx("form__entrance")}>
+                    <div className={cx("form__subtitle")}>
+                        Или войти через
+                    </div>
+
+                    <div className={cx("form__log-in")}>
+                            <Link to={"/"} className={cx("form__icon")}>
+                                <Icon name='telegram'/>
+                            </Link>
+
+                            <Link to={"/"} className={cx("form__icon")}>
+                                <Icon name='google'/>
+
+                            </Link>
+
+                            <Link to={"/"} className={cx("form__icon")}>
+                                <Icon name='github'/>
+                            </Link>
+                    </div>
                 </div>
-
-                <div className={cx("form__log-in")}>
-                        <Link to={"/"} className={cx("form__icon")}>
-                            <Icon name='telegram'/>
-                        </Link>
-
-                        <Link to={"/"} className={cx("form__icon")}>
-                            <Icon name='google'/>
-
-                        </Link>
-
-                        <Link to={"/"} className={cx("form__icon")}>
-                            <Icon name='github'/>
-                        </Link>
-                </div>
-            </div>
-        </form>
+            </form>
+        </>
     )
 }
