37e715300fafe5c03121045a55d8551662b81ccc
Andrey <andre.kozloff2004@gmail.com>
2024-01-14 00:42:19 +0300

    ASF-7: создал первоначальные файлы для компонента Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+// import { Icon } from 'src/ui/icon/Icon';
+// import { useState } from 'react';
+// import style from "./header.module.scss"
+// import cnBind from 'classnames/bind'
+
+// const cx = cnBind.bind(style)
+
+export const Header = () => {
+    return (
+        <div>Header</div>
+    )
+}
