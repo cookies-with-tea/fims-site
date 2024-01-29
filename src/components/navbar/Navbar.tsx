0cc6cf3835ec8db3ea5ddaf75152a2fe2e285a13
Andrey <andre.kozloff2004@gmail.com>
2024-01-29 03:17:15 +0300

    ASF-8: изменил нейминг компонента

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 // import { NavLink } from "react-router-dom"
 import style from "./navbar.module.scss"
 import cnBind from 'classnames/bind'
-import { Popover } from "src/ui/popover/popover"
+import { Tooltip } from "src/ui/tooltip/Tooltip"
 
 const cx = cnBind.bind(style)
 
 export const Navbar = () => {
-    // DEBT: до реализации компонента popover данный компонент приостановлен
+    // DEBT: до реализации компонента Tooltip данный компонент приостановлен
     const navigation = [
         {name:"Главная", path:"/"},
         {name:"Новинки", path:"/new-anime"},
@@ -21,7 +21,7 @@ export const Navbar = () => {
                     navigation.map((item, index) => (
                         <li key={index} className={cx("menu__item")}>
                             {/* <NavLink to={item.path} className={cx("menu__link")}> */}
-                                {index != 2 ? item.name : <Popover content="Аниме" trigger="click" position="top"> {item.name}</Popover>}
+                                {index != 2 ? item.name : <Tooltip content="Аниме" trigger="click" position="top"> {item.name}</Tooltip>}
                             {/* </NavLink> */}
                         </li>
                     ))
