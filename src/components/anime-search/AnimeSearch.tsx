f91c40b730a7533b4c121b5c9da24b204a18d3a8
Andrey <andre.kozloff2004@gmail.com>
2024-03-14 21:32:47 +0300

    ASF-9: сделана  функция,  которая отвечает за выделение слов при совпадение

--- src/components/anime-search/AnimeSearch.tsx
+++ src/components/anime-search/AnimeSearch.tsx
@@ -4,6 +4,7 @@ import { useModal } from "src/hooks/modal/Modal.tsx";
 import { Dialog } from "src/ui/dialog/dialog.tsx";
 import {Input} from "src/ui/input/input.tsx";
 import {useDebounce} from "src/hooks/debounce/Debounce.tsx";
+import {markText} from "src/utils/markText.tsx";
 import axios from "axios";
 import style from "./animeSeacrh.module.scss"
 import cnBind from 'classnames/bind'
@@ -11,34 +12,37 @@ import cnBind from 'classnames/bind'
 const cx = cnBind.bind(style)
 const url = (keyword: string) => `https://kinopoiskapiunofficial.tech/api/v2.2/films?keyword=${keyword}`
 const option = {
- headers: {
-   'X-API-KEY': '4dc0fb6b-92e7-4e5d-b3c6-960b4ce6443d',
-   'Content-Type': 'application/json',
- },
+  headers: {
+    'X-API-KEY': '4dc0fb6b-92e7-4e5d-b3c6-960b4ce6443d',
+    'Content-Type': 'application/json',
+  },
 }
-
+interface DataFilms {
+  kinopoiskId: number
+  nameRu: string
+  year: number
+}
 export const AnimeSearch = () => {
   const [visible, changeVisible] = useModal();
   const [dataFilms, setDataFilms] = useState([])
   const [search, setSearch] = useState("")
-  const ref = useRef(null)
-  ref.current = search
+  const searchRef = useRef<string>(search)
+  searchRef.current = search
 
   const debounce = useCallback(useDebounce(() => getDataFilms() , 2000), [])
   const onClearValue = (): void => {
-   setSearch("" )
+    setSearch("" )
   }
 
   const getDataFilms = async () => {
     try {
-      const data = await (await (axios.get(url(ref.current), option))).data.items.slice(0, 9).filter(item => item.nameRu != null)
+      const data = await (await (axios.get(url(searchRef.current), option))).data.items.slice(0, 9).filter(({ nameRu }: DataFilms) => nameRu != null)
       setDataFilms(data)
       console.log(data)
     } catch (error) {
       console.log(`${error} --- error`)
     }
   }
-
   // DEBT: в дальнешем пересмотреть надобность функции onClearValue
   const onValueChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setSearch(event.target.value)
@@ -55,6 +59,7 @@ export const AnimeSearch = () => {
           visible={visible}
           onClose={changeVisible}
           className={cx("dialog__content")}
+          title="Поиск"
         >
           <Input
             name="search"
@@ -65,21 +70,21 @@ export const AnimeSearch = () => {
             onClearValue={onClearValue}
           />
           <ul className={cx("anime-search__menu")}>
-            {dataFilms.map(item => (
-              <li key={item.kinopoiskId} className={cx("anime-search__item")}>
+            {dataFilms.map(( {kinopoiskId, nameRu, year}: DataFilms) => (
+              <li key={kinopoiskId} className={cx("anime-search__item")}>
                 <Icon name="video" className={cx("anime-search__icon-video")}/>
                 <div className={cx("anime-search__content")}>
-                  <div className={cx("anime-search__name")}>
-                    {item.nameRu}
+                  <div
+                    className={cx("anime-search__name")}
+                    dangerouslySetInnerHTML={{__html: markText({repString: searchRef.current, fullString: nameRu})}}>
                   </div>
                   <div className={cx("anime-search__year")}>
-                    {item.year}
+                    {year}
                   </div>
                 </div>
               </li>
             ))}
           </ul>
-
         </Dialog>
       </>
   );
