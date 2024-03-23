f33f76317129d1e3581e177ddb8de47372469f7e
Andrey <andre.kozloff2004@gmail.com>
2024-03-23 16:46:31 +0300

    ASF-7: стилизовал кнопку button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import {Logo} from "src/components/logo/Logo"
 import { Navbar } from "src/components/navbar/Navbar"
 import {AnimeSearch} from "src/components/anime-search/AnimeSearch.tsx";
 import {Button} from "src/ui/button/Button.tsx";
+import {Icon} from "src/ui/icon/Icon.tsx";
 import style from "./header.module.scss"
 import cnBind from 'classnames/bind'
 
@@ -15,12 +16,13 @@ export const Header = () => {
 
               <Navbar/>
 
+
               <AnimeSearch/>
 
               <Button
                 className={cx("header__button")}
-                size={"sm"}
                 href={"/login"}
+                prefixIcon={<Icon name={"user"} className={cx("header__icon")}/>}
               >
                 Вход
               </Button>
