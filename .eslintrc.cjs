a06072100da34a1e2ab3764d099a687b0bda9b11
Alex <57665901+miirsery@users.noreply.github.com>
2024-03-26 19:45:53 +0700

    feat(ASF-12): Настроить проект (#56)
    * ASF-12 | Настройка проекта
    
    * ASF-12 | Настройка проекта
    
    * ASF-12 | Настройка проекта

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,80 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'prettier',
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: { jsx: true },
+    sourceType: 'module',
   },
+  plugins: ['react-refresh', '@typescript-eslint', 'react-hooks', 'react'],
+  globals: {
+    process: true,
+  },
+  rules: {
+    "react/jsx-first-prop-new-line": [2, "multiline"],
+    "react/jsx-newline": [2, { "prevent": false  }],
+    "react/jsx-max-props-per-line": [
+      2,
+      { "maximum": 1, "when": "multiline" }
+    ],
+    "react/jsx-equals-spacing": [2, "never"],
+    "react/jsx-indent": [2, 2, {"checkAttributes": true, "indentLogicalExpressions": true}],
+    "react/jsx-indent-props": [2, 2],
+    "react/jsx-sort-props": [2, {
+      "callbacksLast": true,
+      "shorthandFirst": true,
+      "shorthandLast": true,
+      "multiline": "ignore",
+      "ignoreCase": true,
+      "noSortAlphabetically": true,
+      "reservedFirst": false,
+      "locale": "auto"
+    }],
+    "react/jsx-closing-bracket-location": [
+      2,
+      "tag-aligned"
+    ],
+    "import/no-anonymous-default-export": "warn",
+    "react/no-unknown-property": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "semi": [
+      "error", "never"
+    ],
+    "quotes": [
+      "error", "single"
+    ],
+    "object-curly-spacing": [
+      "error", "always", { "objectsInObjects": true }],
+    "react/jsx-no-target-blank": "off",
+    "max-len": [
+      "error",
+      {
+        "code": 120,
+        "tabWidth": 2,
+        "ignoreComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true
+      }
+    ],
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+    "@typescript-eslint/no-var-requires": 1,
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
 }
