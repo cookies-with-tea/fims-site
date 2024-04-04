5bf0777455f2fd1d29a949ccb97befb4d48ad110
Andrey <andre.kozloff2004@gmail.com>
2024-04-04 16:07:14 +0300

    ASF-5: создан компонент слайдера

---
+++ src/components/best-films/BestFilms.tsx
@@ -0,0 +1,11 @@
+// import {style} from "./style.module.scss"
+// import cnBind from 'classnames/bind'
+import { SwiperSlider } from '@components/swiper-slider/SwiperSlider'
+
+export  const BestFilms = () =>  {
+  return (
+    <section>
+      <SwiperSlider/>
+    </section>
+  )
+}
