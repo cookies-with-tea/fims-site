51ff61fb4c3c0c137aa075c06f8ca8202cdc9021
Andrey <andre.kozloff2004@gmail.com>
2024-04-07 15:51:34 +0300

    ASF-5: типизировал слайдер

--- src/components/move-films/MoveSlider.tsx
+++ src/components/move-films/MoveSlider.tsx
@@ -2,10 +2,39 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 // Import Swiper styles
-import 'swiper/css'
+import 'swiper/scss'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films'
+const option = {
+  headers: {
+    'X-API-KEY': '4dc0fb6b-92e7-4e5d-b3c6-960b4ce6443d',
+    'Content-Type': 'application/json',
+  },
+}
+
+type DataSlider = {
+  kinopoiskId?: number
+  posterUrl?: string
+}
 
 export const MoveSlider = () => {
+  const [dateSlider, setDateSlider] = useState<object[]>([])
 
+  useEffect(() => {
+    const getDateSlides = async () => {
+      try {
+        const data: object[] = await (await (axios.get(url, option))).data.items
+        setDateSlider(data)
+      } catch (error) {
+        console.log(`${error} --- error`)
+      }
+    }
+
+    getDateSlides()
+  }, [])
   return (
     <Swiper
       spaceBetween={50}
@@ -13,13 +42,13 @@ export const MoveSlider = () => {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-
-      <SwiperSlide> Slide 2 </SwiperSlide>
-
-      <SwiperSlide>Slide 3</SwiperSlide>
-
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {dateSlider.map(({ kinopoiskId, posterUrl }: DataSlider) => (
+        <SwiperSlide key={kinopoiskId}>
+          <Link to={'/'}>
+            <img src={posterUrl} alt={'картинка фильма'}/>
+          </Link>
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 }
