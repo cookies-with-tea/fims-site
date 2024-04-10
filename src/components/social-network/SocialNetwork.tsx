dc0e31a13b8d690af12c08bf98d51b54036e861f
Andrey <andre.kozloff2004@gmail.com>
2024-04-10 19:25:11 +0300

    ASF-5: добавил пропы позиционирования компонента соц сетей

--- src/components/social-network/SocialNetwork.tsx
+++ src/components/social-network/SocialNetwork.tsx
@@ -1,22 +1,34 @@
 import { Icon } from '@/ui'
+import { Link } from 'react-router-dom'
 import style from './style.module.scss'
 import cnBind from 'classnames/bind'
 
 const cx = cnBind.bind(style)
 
-export const SocialNetwork = () => {
+interface SocialNetwork {
+  position?: 'row' | 'column'
+  className?: string
+}
+
+export const SocialNetwork = ({ position = 'row', className = '' }: SocialNetwork) => {
   return(
-    <div className={cx('social-network')}>
-      <div className={cx('social-network__item')}>
-        <Icon name={'insta'} className={cx('social-network__icon')} />
+    <div className={cx('social-network', position)}>
+      <div className={cx('social-network__item', className)}>
+        <Link to={'/'} className={cx('social-network__link')}>
+          <Icon name={'insta'} className={cx('social-network__icon')} />
+        </Link>
       </div>
 
       <div className={cx('social-network__item')}>
-        <Icon name={'linkedin'} className={cx('social-network__icon')} />
+        <Link to={'/'} className={cx('social-network__link')}>
+          <Icon name={'linkedin'} className={cx('social-network__icon')} />
+        </Link>
       </div>
 
       <div className={cx('social-network__item')}>
-        <Icon name={'telegram-social'} className={cx('social-network__icon')} />
+        <Link to={'/' } className={cx('social-network__link')}>
+          <Icon name={'telegram-social'} className={cx('social-network__icon')} />
+        </Link>
       </div>
     </div>
   )
