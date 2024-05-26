17d9ec923872c0c4cbfa32dd03bfacd832b64cd2
Andrey <andre.kozloff2004@gmail.com>
2024-05-26 15:20:30 +0300

    feat/ASF-15: исправил название файла

---
+++ src/pages/base/anime-list/ui/AnimeList.tsx
@@ -0,0 +1,11 @@
+import { AnimeFilters } from '@components/anime-filters'
+
+export const AnimeList = () => {
+  return (
+    <div className={'anime-list'}>
+      <AnimeFilters />
+
+      <h2>Страница аниме!! ^_^</h2>
+    </div>
+  )
+}
